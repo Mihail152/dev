b8e92badf0412aab86a37c8165bd02f73391760b
Zalim <zalim@pineparks.com>
2024-11-15 21:48:47 +0300

    fix

--- wp-content/themes/testTheme/functions.php
+++ wp-content/themes/testTheme/functions.php
@@ -1,8 +1,11 @@
 <?php
 function load_style_script()
 {
+    // Подключение стилей
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_scripts('script', get_template_directory_uri() . '/js/script.js');
+
+    // Подключение скриптов
+    wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array(), null, true);
 }
 
 add_action('wp_enqueue_scripts', 'load_style_script');
