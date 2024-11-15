4cb35551bbb8b79a4aa48b60322ec875a2afbb15
Zalim <zalim@pineparks.com>
2024-11-15 21:44:50 +0300

    fix

--- wp-content/themes/testTheme/functions.php
+++ wp-content/themes/testTheme/functions.php
@@ -1,3 +1,8 @@
 <?php
-wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
-wp_enqueue_scripts('script', get_template_directory_uri() . '/js/script.js');
+function load_style_script()
+{
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_scripts('script', get_template_directory_uri() . '/js/script.js');
+}
+
+add_action('wp_enqueue_scripts', 'load_style_script');
--- wp-content/themes/testTheme/index.php
+++ wp-content/themes/testTheme/index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>test .php</title>
+    <?php wp_head();?>
 </head>
 <body>
     <h1>test</h1>
+    <?php wp_footer();?>
 </body>
 </html>
